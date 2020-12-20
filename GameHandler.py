7d7ace3ede635194af524c4a9c66f5f1ba5c0541
Mahdi Ahmed <ahmedmahdi.5491@googlemail.com>
2020-12-20 23:19:18 +0100

    first playable somewhat version

--- GameHandler.py
+++ GameHandler.py
@@ -49,28 +49,42 @@ class GameHandler:
         #    board.draw_selected(arr[0], arr[1])                #vlt später nützzlich
 
         
+
+        # self.selected hat als 3. Index, ob es ein piece genommen hat oder nicht, 0 ist nein 1 ist ja
         
         if board_arr[y][x] != 0:
 
             try:        
                 if board_arr[y-1][x-1] == 0 and board_arr[y][x].get_player1() == True and self.current_player == True:       #ist frei links oben und ist spieler 1
                     self.selected.append([x-1, y-1])
+                elif board_arr[y-1][x-1] != 0 and board_arr[y][x].get_player1() == True and self.current_player == True:
+                    if board_arr[y-2][x-2] == 0 and board_arr[y-1][x-1].get_player1() == False:
+                        self.selected.append([x-2,y-2])
             except: pass
 
             try:
-                if board_arr[y+1][x-1] == 0 and board_arr[y][x].get_player1() == False and self.current_player == False:
+                if board_arr[y+1][x-1] == 0 and board_arr[y][x].get_player1() == False and self.current_player == False:       #frei spieler 2
                     self.selected.append([x-1,y+1])
+                elif board_arr[y+1][x-1] != 0 and board_arr[y][x].get_player1() == False and self.current_player == False:
+                    if board_arr[y+2][x-2] == 0 and board_arr[y+1][x-1].get_player1() == True:
+                        self.selected.append([x-2, y+2])
             except: pass
 
             try:
                 if board_arr[y-1][x+1] == 0 and board_arr[y][x].get_player1() == True and self.current_player == True:       #ist frei rechts oben und ist spieler 1
                     self.selected.append([x+1, y-1])
+                elif board_arr[y-1][x+1] != 0 and board_arr[y][x].get_player1() == True and self.current_player == True:
+                    if board_arr[y-2][x+2] == 0 and board_arr[y-1][x+1].get_player1() == False:
+                        self.selected.append([x+2, y-2])
             except: pass
 
             try:
-                if board_arr[y+1][x+1] == 0 and board_arr[y][x].get_player1() == False and self.current_player == False:
+                if board_arr[y+1][x+1] == 0 and board_arr[y][x].get_player1() == False and self.current_player == False:       #frei spieler 2
                     self.selected.append([x+1, y+1])
-            except: pass    
+                elif board_arr[y+1][x+1] != 0 and board_arr[y][x].get_player1() == False and self.current_player == False:
+                    if board_arr[y+2][x+2] == 0 and board_arr[y+1][x+1].get_player1() == True:
+                        self.selected.append([x+2, y+2])
+            except: pass
         
         if self.selected != None:
             for coords in self.selected:
@@ -92,10 +106,23 @@ class GameHandler:
 
         dx =  moveAt[1] - origin[1]
         dy = moveAt[0] - origin[0]
+
+        if dx <= -2 and dy <= -2:
+            board.set_new_coords([origin[0] - 1, origin[1] - 1], 0)
+        
+        if dx <= -2 and dy >= 2:
+            board.set_new_coords([origin[0] + 1, origin[1] - 1], 0)
+
+        if dx >= 2 and dy <= -2:
+            board.set_new_coords([origin[0] - 1, origin[1] + 1], 0)
+        
+        if dx >= 2 and dy >= 2:
+            board.set_new_coords([origin[0] + 1, origin[1] + 1], 0)
+
         newX = origin[1] + dx
         newY = origin[0] + dy
         piece.set_coords([newY, newX])
-        print(piece.get_coords())
+        
         board.set_new_coords([newY, newX], piece)
         board.set_new_coords([origin[0], origin[1]], 0)
         
@@ -105,6 +132,8 @@ class GameHandler:
 
         self.current_player = not self.current_player
         return
+    
+    
         
 
     def check_moves(self): return
